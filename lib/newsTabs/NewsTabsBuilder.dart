bc551506172b759d6ddfe5dec4b9ee543e941d18
Reham Tarek <rehamtarek666666@gmail.com>
2021-09-13 03:14:10 +0200

    added New Tabs component

---
+++ lib/newsTabs/NewsTabsBuilder.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:untitled/api/ApiServices.dart';
+import 'package:untitled/model/SourcesResponse.dart';
+
+import 'AllTabsItems.dart';
+
+class NewsTabsBuilder extends StatefulWidget {
+  final String selectedCategory;
+
+  NewsTabsBuilder({@required this.selectedCategory});
+
+  @override
+  _NewsTabsBuilderState createState() => _NewsTabsBuilderState();
+}
+
+class _NewsTabsBuilderState extends State<NewsTabsBuilder> {
+  Future<SourcesResponse> newsSourceResponse;
+  @override
+  void initState() {
+    super.initState();
+    newsSourceResponse = getNewsTabs(widget.selectedCategory);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<SourcesResponse>(
+        future: newsSourceResponse,
+        builder: (buildContext, snapShot) {
+          if (snapShot.hasData) {
+            return AllTabsItems(snapShot.data.sources);
+          } else if (snapShot.hasError) {
+            return Center(
+              child: FloatingActionButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: Text('Retry'),
+              ),
+            );
+          }
+          return Center(child: CircularProgressIndicator());
+        });
+  }
+}
